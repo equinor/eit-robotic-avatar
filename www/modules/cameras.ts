b8cc4b22e0ce29a4af749e674ef3f8de20764bdc
Ole Martin Gjersvik <olgj@equinor.com>
2022-07-26 09:50:41 +0200

    Updated camera

--- www/modules/cameras.ts
+++ www/modules/cameras.ts
@@ -1,5 +1,5 @@
-const leftCam = "171576bfc26c9ce37f0881de5c44ef2dc7d7b82d92a4e382f1d7b94957573e6f";
-const rightCam = "c13e97362e3e5bc794c2f8d176fb475da32f32f27c7e09fdc2d9565187511b04";
+const leftCam = "a8b68e4db341457caec73e5058c28c6f2278a21d5381171f40f515af7d649f87";
+const rightCam = "1eaf34dd447b89454553a211b0e7adb038ab1bff3e2066a462e043d5074d0b4d";
 
 export async function loadCams(leftId = leftCam, rightId = rightCam): Promise<{ left: MediaStream; right: MediaStream; }>{
     // log list of devices to console.
