06c727cce2c5ae0c597a16bea1e20facd9f7b8cc
Ole Martin Gjersvik <olgj@equinor.com>
2022-08-14 12:59:51 +0200

    Demo quick fixes (#32)

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ use tokio::net::UdpSocket;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Setup udp
     let sock = Arc::new(UdpSocket::bind("0.0.0.0:0".parse::<SocketAddr>().unwrap()).await?);
-    let remote_addr = "10.52.118.146:6666".parse::<SocketAddr>().unwrap();
+    let remote_addr = "10.52.118.223:6666".parse::<SocketAddr>().unwrap();
     sock.connect(remote_addr).await?;
 
     // build our application with a single route
@@ -42,7 +42,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             Addr::V6(_) =>None
         }
     }).collect();
-    subject_alt_names.push("10.52.115.15".to_string());
+    subject_alt_names.push("10.52.115.128".to_string());
 
     println!("Creating cert for: {}", subject_alt_names.join(" "));
 
